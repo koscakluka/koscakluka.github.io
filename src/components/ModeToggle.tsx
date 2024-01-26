d524d66433bf97d4efa21bfdb013c20917ec45ea
Luka Košćak <koscak.luka@gmail.com>
2024-01-26 16:47:45 +0200

    Add Service Design Session and Introduction
    When read on the page without the header it looked weird so it was redesigned to
    include a header that at least has some framing.
    
    All elements were defined only for the session probably it will be worth
    exporting them and importing them for other sessions.
    
    The introduction is nothing fancy, dynamic behaviour will be added
    later.

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export function ModeToggle() {
+export function ModeToggle({className}: {className?: string}) {
   const [theme, setThemeState] = React.useState<
     "theme-light" | "dark" | "system"
   >("system")
@@ -29,7 +29,7 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger className={className} asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:text-neutral-50" />
