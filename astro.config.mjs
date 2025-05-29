2870baf7d472c1a11ed8b54f08aa2da92cc338e1
Luka Košćak <koscak.luka@gmail.com>
2025-05-29 14:48:50 +0300

    build: setup astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({applyBaseStyles: false}), mdx()],
-  site: 'https://lukakoscak.me'
+  integrations: [react(), tailwind({ applyBaseStyles: false }), mdx(), icon()],
+  site: "https://lukakoscak.me",
 });
