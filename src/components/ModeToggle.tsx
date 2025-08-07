5be845fe902958c01c13269a044471349f72411d
Luka Košćak <koscak.luka@gmail.com>
2025-08-07 22:45:23 +0200

    feat: match nav styling; add footer

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -33,7 +33,7 @@ export function ModeToggle({ className }: { className?: string }) {
         <Button
           variant="outline"
           size="icon"
-          className="bg-background dark:bg-neutral-950"
+          className="bg-background text-primary hover:text-primary border-0"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:text-neutral-50" />
