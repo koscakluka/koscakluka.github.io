26032b83860ce586d0eb04c4b35157c092e9ad23
Luka Košćak <koscak.luka@gmail.com>
2025-08-07 22:49:00 +0200

    feat: add gist descriptions

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,13 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const gists = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.mdx', base: "./src/data/gists" }),
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-    }),
+  loader: glob({ pattern: "**/[^_]*.mdx", base: "./src/data/gists" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string().optional(),
+  }),
 });
 
 export const collections = { gists };
